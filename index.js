729b15bfd9eeac1c12d0c2dcc67e5d7fc81fbc69
urtblw <dgoskiy@gmail.com>
2024-05-11 19:18:07 +0700

    1.1.0

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-// import express from "express"
-// import mongoose from "mongoose"
-// import multer from "multer"
-// import cors from "cors"
-// import { postCreateValidation, loginValidation, registerValidation } from "./validations.js"
-// import { checkAuth, handleValidationErrors } from './utils/index.js'
-// import { PostController, UserController } from './controllers/index.js'
-// mongoose.set('strictQuery', true)
-// mongoose
-//   .connect('mongodb+srv://iykisoba:artur42r@cluster0.reryft7.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
-//   // .connect('mongodb+srv://dgoskiy:artur42r@cluster0.tohy41e.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
+import express from 'express'
+import mongoose from "mongoose"
+import multer from "multer"
+import cors from 'cors'
+import { postCreateValidation, loginValidation, registerValidation } from "./validations.js"
+import { checkAuth, handleValidationErrors } from './utils/index.js'
+import { PostController, UserController } from './controllers/index.js'
+mongoose.set('strictQuery', true)
+mongoose
+  .connect('mongodb+srv://iykisoba:artur42r@cluster0.reryft7.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
+// .connect('mongodb+srv://dgoskiy:artur42r@cluster0.tohy41e.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
 
-//   .then(() => console.log('DB ok'))
-//   .catch((err) => console.log("DB error", err))
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log("DB error", err))
 
-// const app = express()
-// const storage = multer.diskStorage({
-//   destination: (_, __, cb) => {
-//     cb(null, 'uploads');
-//   },
-//   filename: (_, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
+const app = express();
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-// const upload = multer({ storage });
+const upload = multer({ storage });
 
-// app.use(express.json())
+app.use(express.json());
+app.use(cors());
 // app.use(cors(
 //   {
 //     origin: ['https://gen-shortcuts.vercel.app'],
@@ -33,51 +34,33 @@
 //     credentials: true
 //   }
 // ));
-// app.get("/", (req, res) => {
-//   res.json("Hello")
-// })
-// app.use('/uploads', express.static('uploads'))
-// app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
-// app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)
-// app.get('/auth/me', checkAuth, UserController.getMe)
-
-// app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-//   res.json({
-//     url: `/uploads/${req.file.originalname}`,
-//   });
-// })
-
-// app.post('/posts', checkAuth, postCreateValidation, PostController.create)
-// // app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create)
-// app.get('/posts', PostController.getAll)
-// app.get('/posts/:id', PostController.getOne)
-// app.delete('/posts/:id', checkAuth, PostController.remove)
-// app.patch('/posts/:id', checkAuth, handleValidationErrors, PostController.update)
-
-
-// // app.post('/auth/ebasit', ebasit)
-// app.listen(4444, (err) => {
-//   if (err) {
-//     return console.log("Ошибка!!!")
-//   }
-//   console.log("Server ОК")
-// })
-
-
-import express from 'express'
-import cors from 'cors'
-
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.json());
-// Enable CORS for all routes
-app.use(cors());
-app.get('/', (req, res) => {
-  res.send('Express server working!')
+app.get("/", (req, res) => {
+  res.json("Hello")
 })
-app.get("/about", (req, res) => {
-    res.json({"message": "Welcome to the about page"})
+app.use('/uploads', express.static('uploads'))
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)
+app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
 })
-app.listen(port, () => {
-    console.log("Sergeant we have a server on the loose...someone catch it");
+
+app.post('/posts', checkAuth, postCreateValidation, PostController.create)
+// выше убрал "handleValidationErrors" app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create)
+app.get('/posts', PostController.getAll)
+app.get('/posts/:id', PostController.getOne)
+app.delete('/posts/:id', checkAuth, PostController.remove)
+app.patch('/posts/:id', checkAuth, handleValidationErrors, PostController.update)
+
+
+// app.post('/auth/ebasit', ebasit)
+
+app.listen(process.env.PORT || 4444, (err) => {
+  if (err) {
+    return console.log("Ошибка!!!")
+  }
+  console.log("Server ОК")
 })
